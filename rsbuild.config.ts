77d75beec2bc72b2dc9883220aea924b6e02715f
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-18 22:32:06 +0600

    create btn file and add config to absolute path

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,22 @@ import { pluginSvelte } from '@rsbuild/plugin-svelte';
 
 export default defineConfig({
   plugins: [pluginSvelte()],
+  html: {
+    favicon: '',
+    title: 'Vibrant',
+    appIcon: '',
+  },
+  source: {
+    alias: (alias) => {
+      return {
+        '@atoms': './src/atoms',
+        '@molecules': './src/molecules',
+        '@organisms': './src/organisms',
+        '@templates': './src/templates',
+        '@pages': './src/pages',
+        '@app': './src/app',
+        '@commons': './src/commons',
+      };
+    },
+  },
 });

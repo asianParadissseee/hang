ef11dc0fbfed68061f3592c939057a587b44f30a
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-18 22:53:50 +0600

    add colors for config on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,svelte}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: "#965CFF",
+        dark: "#2C313A",
+        green: "#4CBD97",
+        gray: "#545454"
+      }
+    },
   },
   plugins: [],
 }
